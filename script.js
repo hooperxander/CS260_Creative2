e285ade1e3d445ad430e013167efc4532be26d3b
Timothy Pope <popet2@byu.edu>
2019-10-25 22:01:46 +0000

    Worked on select/replace functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let app = new Vue({
   el: '#app',
   data: {
     cards: {},
+    deck_id: '',
     loaded: false,
   },
 
@@ -13,6 +14,11 @@ let app = new Vue({
         .then(response => {
           console.log(response)
           this.cards = response.data.cards;
+          this.deck_id = response.data.deck_id;
+          this.cards.forEach(function(card) {
+              card.selected = false;
+              card.class = "";
+          })
           this.loaded = true;
           return true;
         })
@@ -20,7 +26,20 @@ let app = new Vue({
           
         });
     },
-   
+      selectCard(card) {
+        this.cards.forEach(function(thisCard) {
+          if (card === thisCard) {
+          if (thisCard.selected) {
+            thisCard.selected = false;
+            thisCard.class = "";
+          }
+          else {
+            thisCard.selected = true;
+            thisCard.class = "selected";
+          }
+        }
+        })
+      }
   },
   computed: {
     
