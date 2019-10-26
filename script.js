94a9956834adff19f8967d0cc9d1555b6f9b9dfd
hooperxander@gmail.com <hooperxander@gmail.com>
2019-10-26 01:59:08 +0000

    variable hand size and fits cards to screen

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ let app = new Vue({
     cards: {},
     deck_id: '',
     loaded: false,
+    numCards: 5,
   },
 
   methods: {
     generateHand() {
       this.loaded = false;
-      axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=5")
+      axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=" + this.numCards)
         .then(response => {
-          console.log(response)
+          console.log("hi");
+          console.log(response);
           this.cards = response.data.cards;
           this.deck_id = response.data.deck_id;
           this.cards.forEach(function(card) {
@@ -23,23 +25,13 @@ let app = new Vue({
           return true;
         })
         .catch(error => {
-          
+
         });
     },
-    replaceSelected() {
-      this.loaded = false;
-      for (let i = 0; i < this.cards.length; i++) {
-        if (this.cards[i].selected) {
-          axios.get("https://deckofcardsapi.com/api/deck/" + this.deck_id + "/draw/?count=1")
-            .then(response => {
-              this.cards[i] = response.data.cards[0];
-            });
-        }
-      }
-    },
+   
   },
   computed: {
-    
+
   },
   watch: {
 
